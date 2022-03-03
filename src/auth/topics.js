4054960bf6c0f6cfd5a16e74df443b1a811d94a8
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-03-03 11:47:28 +0530

    Change export pattern

--- src/auth/topics.js
+++ src/auth/topics.js
@@ -1,34 +1,23 @@
-const LOGIN_WITH_EMAIL = "Login with email and password";
+export const LOGIN_WITH_EMAIL = "Login with email and password";
 // data: { email, password }
 
-const LOGIN_WITH_GOOGLE = "Login with google";
+export const LOGIN_WITH_GOOGLE = "Login with google";
 // data: None
 
-const LOGIN_ANONYMOUSLY = "Login anonymously";
+export const LOGIN_ANONYMOUSLY = "Login anonymously";
 // data: None
 
-const LOGOUT_USER = "Logout current user";
+export const LOGOUT_USER = "Logout current user";
 // data: None
 
-const AUTH_LOGIN_ERROR = "Error occured when trying to Login";
+export const AUTH_LOGIN_ERROR = "Error occured when trying to Login";
 // data: error
 
-const SIGNUP_WITH_GOOGLE = "Signup with google";
+export const SIGNUP_WITH_GOOGLE = "Signup with google";
 // data: None
 
-const SIGNUP_WITH_EMAIL = "Signup with email and password";
+export const SIGNUP_WITH_EMAIL = "Signup with email and password";
 // data: { email, password }
 
-const AUTH_SIGNUP_ERROR = "Error occured when trying to Signup";
+export const AUTH_SIGNUP_ERROR = "Error occured when trying to Signup";
 // data: error
-
-export {
-  LOGIN_WITH_EMAIL,
-  LOGIN_WITH_GOOGLE,
-  LOGIN_ANONYMOUSLY,
-  LOGOUT_USER,
-  AUTH_LOGIN_ERROR,
-  SIGNUP_WITH_GOOGLE,
-  SIGNUP_WITH_EMAIL,
-  AUTH_SIGNUP_ERROR,
-};
