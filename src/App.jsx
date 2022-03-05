a6f9321f3fd8bbcf698f288cc311943e180124ea
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-03-05 10:02:29 +0530

    Remove auto anonymous login on auth change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import Header from "./components/Header";
 import "./App.css";
-import { useCurrentUser } from "./auth/hooks";
-import { useEffect } from "react";
-import { publish } from "./topic-manager";
-import { LOGIN_ANONYMOUSLY } from "./auth/topics";
 
 function App() {
-  const user = useCurrentUser();
-
-  useEffect(() => {
-    if (user === null) {
-      publish(LOGIN_ANONYMOUSLY);
-    }
-  }, [user]);
-
   return (
     <div>
       <Header />
